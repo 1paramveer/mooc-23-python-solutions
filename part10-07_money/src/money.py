1097097e0457e0fe960fa789c51215bfd3201090
Paramveer <paramsingh1205@outlook.com>
2024-01-09 13:36:28 +0530

    second commit

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,52 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        return f"{self.__euros}.{self.__cents:02d} eur"
+
+    def __eq__(self, another):
+        return (self.__euros + self.__cents) == (another.__euros + another.__cents)
+    
+    def __gt__(self, another):
+        return (self.__euros + self.__cents) > (another.__euros + another.__cents)
+    
+    def __ne__(self, another):
+        return (self.__euros + self.__cents) != (another.__euros + another.__cents)
+
+    def __add__(self,another):
+        total_euros = self.__euros + another.__euros
+        total_cents = self.__cents + another.__cents
+        if total_cents >= 100:
+            div_euros = int((str(total_cents))[:1])
+            total_cents = int((str(total_cents))[1:])
+            total_euros += div_euros
+        return Money(total_euros, total_cents)
+    
+    def __sub__(self,another):
+        total_euros = self.__euros - another.__euros
+        total_cents = self.__cents - another.__cents
+
+        if total_cents < 0:
+            total_euros -= 1
+            total_cents += 100
+
+        if total_euros < 0:
+            raise ValueError
+        
+        return Money(total_euros, total_cents)
+            
+
+if __name__ == "__main__":
+    e1 = Money(4, 5)
+    e2 = Money(2, 95)
+
+    e3 = e1 + e2
+    e4 = e1 - e2
+
+    print(e3)
+    print(e4)
+
+    e5 = e2-e1
--- part10-08_simple_date/src/simple_date.py
+++ part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,45 @@
 # WRITE YOUR SOLUTION HERE:
 
+class SimpleDate:
+    def __init__(self,day: int, month: int, year: int):
+        self.__day = day
+        self.__month = month
+        self.__year = year
+
+    def __eq__(self, other):
+        return (self.__day, self.__month, self.__year) == (other.__day, other.__month, other.__year)
+    
+    def __ne__(self, other):
+        return (self.__day, self.__month, self.__year) != (other.__day, other.__month, other.__year)
+    
+    def __lt__(self, other):
+        if self.__year != other.__year:
+            return self.__year < other.__year
+        elif self.__month != other.__month:
+            return self.__month < other.__month
+        else:
+            return self.__day < other.__day
+    
+    def __gt__(self, other):
+        if self.__year != other.__year:
+            return self.__year > other.__year
+        elif self.__month != other.__month:
+            return self.__month > other.__month
+        else:
+            return self.__day > other.__day
+
+    def __str__(self):
+        return f"{self.__day}.{self.__month}.{self.__year}"
+
+if __name__ == "__main__":
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(28, 12, 1985)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d1)
+    print(d2)
+    print(d1 == d2)
+    print(d1 != d2)
+    print(d1 == d3)
+    print(d1 < d2)
+    print(d1 > d2)
